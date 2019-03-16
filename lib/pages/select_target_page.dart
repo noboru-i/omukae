47e8bd4e5ced6621f56f86cae84116b76685d0a2
noboru-i <ishikura.noboru@gmail.com>
2019-03-16 17:05:24 +0900

    show map

--- lib/pages/select_target_page.dart
+++ lib/pages/select_target_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class SelectTargetPage extends StatelessWidget {
   @override
@@ -7,21 +10,46 @@ class SelectTargetPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('目的地の選択'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              padding: EdgeInsets.all(20.0),
-              color: Colors.lightBlue[100],
-              onPressed: () {
+      body: MapContainer(),
+//      body: Center(
+//        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          children: <Widget>[
+//            MapContainer(),
+//            RaisedButton(
+//              padding: EdgeInsets.all(20.0),
+//              color: Colors.lightBlue[100],
+//              onPressed: () {},
+//              child: Text('選択する'),
+//            ),
+//          ],
+//        ),
+//      ),
+    );
+  }
+}
 
-              },
-              child: Text('選択する'),
-            ),
-          ],
-        ),
-      ),
+class MapContainer extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MapContainerState();
+}
+
+class MapContainerState extends State<MapContainer> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      mapType: MapType.hybrid,
+      initialCameraPosition: _kGooglePlex,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
     );
   }
 }
