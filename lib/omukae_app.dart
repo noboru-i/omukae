f6eeee279c72d0ef13fc2e7d6417f137bc82724d
noboru-i <ishikura.noboru@gmail.com>
2019-04-28 14:23:44 +0900

    enable receiving push notification

--- lib/omukae_app.dart
+++ lib/omukae_app.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:geofencing/geofencing.dart';
 import 'package:omukae/pages/welcome_page.dart';
+import 'package:omukae/util/local_notification_util.dart';
 
 class OmukaeApp extends StatefulWidget {
   @override
@@ -11,18 +13,41 @@ class OmukaeApp extends StatefulWidget {
 
 class _OmukaeAppState extends State<OmukaeApp> {
   static FirebaseAnalytics analytics = FirebaseAnalytics();
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
 
   @override
   void initState() {
     super.initState();
 
     _initPlatformState();
+    _initFirebaseMessaging();
   }
 
   Future<void> _initPlatformState() async {
     print('Initializing...');
     await GeofencingManager.initialize();
     print('Initialization done');
+  }
+
+  _initFirebaseMessaging() {
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+      const IosNotificationSettings(sound: true, badge: true, alert: true),
+    );
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      print("firebase token: " + token);
+    });
   }
 
   @override
